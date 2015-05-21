07e8fd5eb5b2ee529970af18303039e223463616
Markus Bosek <markus.bosek@stud.uni-regensburg.de>
2015-05-21 20:52:09 +0200

    added cube script
    added script to freeze a cube on the floor and unfreeze it in the air

---
+++ PSMG_SS_2015_Antichamber/Assets/Scripts/CubeScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeScript : MonoBehaviour {
+
+	Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	//Freezes the cube if it hits the floor
+	void OnTriggerStay(Collider other){
+		if (!other.isTrigger) {
+			rb.constraints = RigidbodyConstraints.FreezeAll;
+			rb.isKinematic = true;
+		}
+	}
+
+	//Unfreezes the cube if it is not touching the floor
+	void OnTriggerExit(Collider other){
+		if (!other.isTrigger) {
+			rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+			rb.isKinematic = false;
+		}
+	}
+}
