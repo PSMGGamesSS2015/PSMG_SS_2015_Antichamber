7786b30ddc222baa15308c6aacd52609440fc223
Markus Bosek <markus.bosek@stud.uni-regensburg.de>
2015-05-21 20:52:42 +0200

    added UI script
    added script to show the player if a cube is pickable or releasable

---
+++ PSMG_SS_2015_Antichamber/Assets/Scripts/UIScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class UIScript : MonoBehaviour {
+
+	Text text;
+
+	// Use this for initialization
+	void Start () {
+		text = GetComponent<Text> ();
+		text.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//shows "Press E to pick" if the player can pick up a cube
+		if (PickTrigger.interact) {
+			text.text = "Press E to pick";
+			text.enabled = true;
+			return;
+		}
+		//shows "Press E to release" if the player can release up a cube
+		if (PickTrigger.releasable) {
+			text.text = "Press E to release";
+			text.enabled = true;
+			return;
+		}
+		text.enabled = false;
+	}
+}
