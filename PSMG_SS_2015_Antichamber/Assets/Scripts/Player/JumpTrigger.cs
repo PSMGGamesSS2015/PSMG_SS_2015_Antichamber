b5e9164577f762febe427068e05ce4c0220be051
Markus Bosek <markus.bosek@stud.uni-regensburg.de>
2015-05-21 20:42:09 +0200

    added grounded script
    added script to determin if the player is grounded

---
+++ PSMG_SS_2015_Antichamber/Assets/Scripts/Player/JumpTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpTrigger : MonoBehaviour {
+
+	public static bool grounded;
+
+	// Use this for initialization
+	void Start () {
+		grounded = false;//the player is grounded if the player is on the floor or on a cube
+	}
+	
+	void OnTriggerStay(Collider other){
+		if (other.gameObject.tag == "Floor" || other.gameObject.tag == "Cube") {
+			grounded = true; 
+		}
+	}
+
+	void OnTriggerExit(Collider other){
+		if (other.gameObject.tag == "Floor" || other.gameObject.tag == "Cube") {
+			grounded = false;
+		}
+	}
+}
